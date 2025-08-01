878220eac444a6fb925d9b54f5fdc51da30f9b37
Md Rony Islam <mdronyislam149@gmail.com>
2025-08-01 10:47:46 +0600

    Some changes occured

--- 1_Two_Sum.cpp
+++ 1_Two_Sum.cpp
@@ -1,10 +1,38 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+#include<algorithm>
 
 using namespace std;
 
+//using two pointer approach
+void twoSum(vector<int> vec, int target){
+    sort(vec.begin(), vec.end());
+    int left = 0, right = vec.size() -1;
+
+    while(left < right){
+        if(vec[left] + vec[right] == target){
+            cout << vec[left] << " " << vec[right] <<endl;
+            cout << left << " " << right << endl;
+            return;
+        }
+        else if(vec[left] + vec[right] < target){
+            left++;
+        }
+        else right --;
+    }
+}
+
 int main()
-{
-    vector<int> twoSum
+{   
+    int n, target;
+    cin >> n >> target;
+
+    vector<int> sum(n);
+    for(int i = 0; i< n; i++){
+        cin >> sum[i];
+    }
+
+    twoSum(sum, target);
+    return 0;
 }
